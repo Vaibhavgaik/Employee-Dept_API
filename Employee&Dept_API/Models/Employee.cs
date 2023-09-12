383c0a9da52c82668c17bf1e1ff0d701e29ca9f5
Vaibhav Gaikwad <gaikwadvaibhav645@gmail.com>
2023-09-12 18:14:41 +0530

    Add project files.

---
+++ Employee&Dept_API/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace Employee_Dept_API.Models
+{
+    public class Employee
+    {
+       public int EmployeeId { get; set; }
+        
+       public string EmployeeName { get; set; }
+
+       public string Department { get; set; }
+
+       public string DateofJoining { get; set; }
+
+       public int MyProperty { get; set; }
+
+       public string PhotoFileName { get; set; }
+
+    }
+}
+
